4a0b51b0aa2ecaf794b4ddaf90371e04e00331ab
RambleTOT <rambletot@gmail.com>
2024-04-03 07:24:18 +0300

    first commit

---
+++ composeApp/src/jvmMain/kotlin/ramble/sokol/chillnto/App.jvm.kt
@@ -0,0 +1,9 @@
+package ramble.sokol.chillnto
+
+import java.awt.Desktop
+import java.net.URI
+
+internal actual fun openUrl(url: String?) {
+    val uri = url?.let { URI.create(it) } ?: return
+    Desktop.getDesktop().browse(uri)
+}
